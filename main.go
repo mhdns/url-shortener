30b6768ce4ab0b2acc321f9c42e20ac274a5f1e0
Muhammad Anas <muhd.anas@outlook.com>
2020-06-23 09:27:45 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", helloworld)
+	http.ListenAndServe(":5000", nil)
+}
+
+func helloworld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World!")
+}
